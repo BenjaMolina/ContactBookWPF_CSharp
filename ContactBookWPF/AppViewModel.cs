dc4a054fd5372f9d2d91e833cd7f328fe1911e94
BenjaMolina <molinasb.94@gmail.com>
2019-10-01 17:15:35 -0500

    Creando Vista y ViewModel Book, Agregando databanding al main principal

---
+++ ContactBookWPF/AppViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ContactBookWPF.Utility;
+using ContactBookWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBookWPF
+{
+    public class AppViewModel : BaseViewModel
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set => OnPropertyChanged(ref _currentView, value);
+        }
+
+        private BookViewModel _bookVM;
+        public BookViewModel BookVM
+        {
+            get => _bookVM;
+            set => OnPropertyChanged(ref _bookVM, value);
+        }
+
+        public AppViewModel()
+        {
+            BookVM = new BookViewModel();
+            CurrentView = BookVM;
+        }
+    }
+}
---
+++ ContactBookWPF/ViewModels/BookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ContactBookWPF.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBookWPF.ViewModels
+{
+    public class BookViewModel : BaseViewModel
+    {
+
+    }
+}
